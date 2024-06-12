e4d336e0864984a7a45c82f7e94303e61aea08ec
dreamsoffcode <akashiiit2001+1@gmail.com>
2024-06-12 15:37:14 +0530

    side bar completed

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -12,9 +12,9 @@ export function Sidebar() {
             </svg>
         </button>
 
-        <aside id="default-sidebar" class = {`fixed top-0 sm:top-0 left-0 z-40 w-56 h-screen transition-transform ${sidebar ? 'translate-x-0': '-translate-x-full'}`} aria-label="Sidebar">
+        <aside id="default-sidebar" class = {`fixed top-0 sm:top-0 left-0 z-40 w-56 h-screen transition-transform ${sidebar ? 'translate-x-0': '-translate-x-full sm:translate-x-0'}`} aria-label="Sidebar">
             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                <ul className="space-y-2 font-medium mt-6">
+                <ul className={`space-y-2 font-medium sm:mt-0 mt-6`}>
                     <li>
                         <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">

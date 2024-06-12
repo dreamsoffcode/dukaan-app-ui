d4697019ae4c0db4dab47031e6d2abeca826c238
dreamsoffcode <akashiiit2001+1@gmail.com>
2024-06-12 15:32:18 +0530

    sidebar completed

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,16 +5,16 @@ export function Sidebar() {
     console.log(sidebar)
     return (<div> 
 
-        <button onClick={()=>setSidebar(true)}data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <button onClick={()=>setSidebar(!sidebar)}data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className={ `inline-flex items-center py-1 mt-2 ms-3 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-2  dark:text-gray-400 dark:focus:ring-gray-600 fixed top-0 bg-none z-50 left-1 sm:hidden ${ sidebar? 'fixed' : 'absolute' }` }>
             <span className="sr-only">Open sidebar</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
         </button>
 
-        <aside id="default-sidebar" class = {sidebar ?  "fixed top-0 left-0 z-40 w-56 h-screen " : "fixed top-0 left-0 z-40 w-56 h-screen transition-transform -translate-x-full sm:translate-x-0"} className="fixed top-0 left-0 z-40 w-56 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+        <aside id="default-sidebar" class = {`fixed top-0 sm:top-0 left-0 z-40 w-56 h-screen transition-transform ${sidebar ? 'translate-x-0': '-translate-x-full'}`} aria-label="Sidebar">
             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                <ul className="space-y-2 font-medium">
+                <ul className="space-y-2 font-medium mt-6">
                     <li>
                         <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
